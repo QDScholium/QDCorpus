fa1bb4d69dc86eed894b6dc666f20755c8686041
Sunny Wan <SunnyWan2020@gmail.com>
2025-01-31 17:46:31 -0500

    indexed around 1000 papers

---
+++ ScholiumCorpus/main.py
@@ -0,0 +1,14 @@
+from ScholiumCorpus.indexer import Indexer
+
+queries = ["Attention Based Transformers", "Encoder-Decoder Models",
+            "Bidirectional Encoders", "DeepSeek", "Chain of Though Prompting",
+            "Positional Encoding", "Positional Embedding", "LLaMA", "GPT","Large Language Models"]
+
+max_docs = 100
+index_name = "scholium-index"
+
+indexer = Indexer(index_name)
+for query in queries:
+    indexer.index_query(query, max_docs)
+    print("Done indexing: {query}")
+print("Done indexing")
